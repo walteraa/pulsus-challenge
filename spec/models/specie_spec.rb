1921c706cb60d53c988eae05fca3961e62d935f1
Walter Alves <walter.arruda.alves@gmail.com>
2021-02-28 04:29:56 -0300

    test: Add initial specs for Specie

--- spec/models/specie_spec.rb
+++ spec/models/specie_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Specie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe '#height_avg' do
+    it { is_expected.to validate_presence_of(:height_avg) }
+  end
 end
