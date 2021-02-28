0f463afd8439e06020a06b9541d0292a217c05b0
Walter Alves <walter.arruda.alves@gmail.com>
2021-02-28 03:17:51 -0300

    chore: update migration and add schema

--- db/migrate/20210228055923_create_people.rb
+++ db/migrate/20210228055923_create_people.rb
@@ -1,10 +1,10 @@
 class CreatePeople < ActiveRecord::Migration[6.1]
   def change
     create_table :people do |t|
-      t.string :name
-      t.string :birthday_year
-      t.integer :height
-      t.integer :mass
+      t.string :name, null: false
+      t.string :birthday_year, null: false
+      t.integer :height, null: false
+      t.integer :mass, null: false
 
       t.timestamps
     end
